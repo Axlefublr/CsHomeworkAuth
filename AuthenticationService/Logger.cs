c64bd448ff770be1d56792cac9587f4c0714db24
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-18 02:04:17 +0800

    add Logger

---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AuthenticationService
+{
+	public class Logger
+	{
+		public void WriteEvent(string eventMessage)
+		{
+			Console.WriteLine(eventMessage);
+		}
+		public void WriteError(string errorMessage)
+		{
+			Console.WriteLine(errorMessage);
+		}
+	}
+}
