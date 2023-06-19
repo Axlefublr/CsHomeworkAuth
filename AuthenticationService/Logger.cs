495894cb50a6649274744b5e2bd3451cfdc7f167
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-19 21:45:20 +0800

    Logger writes to file

--- AuthenticationService/Logger.cs
+++ AuthenticationService/Logger.cs
@@ -1,16 +1,30 @@
 using System;
+using System.IO;
 
 namespace AuthenticationService
 {
 	public class Logger : ILogger
 	{
+		private static readonly string logsDir = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+		private readonly string currentLogsDir;
+		private readonly string eventFile;
+		private readonly string errorFile;
+		public Logger() {
+			currentLogsDir = Path.Combine(logsDir, GenerateDTString());
+			Directory.CreateDirectory(currentLogsDir);
+			eventFile = Path.Combine(currentLogsDir, "events.txt");
+			errorFile = Path.Combine(currentLogsDir, "errors.txt");
+		}
+
+		private static string GenerateDTString() => DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss");
+
 		public void WriteEvent(string eventMessage)
 		{
-			Console.WriteLine(eventMessage);
+			File.AppendAllText(eventFile, eventMessage + '\n');
 		}
 		public void WriteError(string errorMessage)
 		{
-			Console.WriteLine(errorMessage);
+			File.AppendAllText(errorFile, errorMessage + '\n');
 		}
 	}
 }
